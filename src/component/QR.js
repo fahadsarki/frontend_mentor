723d14306e6a157fe211088b35b665114cb65d21
fahadsarki <fahadsarki@gmail.com>
2022-07-21 10:04:58 +0100

    qr code

---
+++ src/component/QR.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './QR.css'
+
+const QR = () => {
+  return (
+    <div className='qr'>
+        <div className='qr-container'>
+            <div className='qr-codebox'>
+                
+            </div>
+            <div className='qr-text'>
+                <h2 className='qr-h1'>Improve your front-end skills by building projects</h2>
+                <p className='qr-p'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default QR
